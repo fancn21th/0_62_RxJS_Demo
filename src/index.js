0701cd028922e010069c92e645e7749836cc5ae9
fancn <fanyj@yikeer.com>
2018-09-09 10:25:18 +0800

    key up event demo

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import { Observable } from 'rxjs'
 import $ from 'jquery'
 
 const btn = $('#btn')
+const txt = $('#txt')
 
 const btnStream$  = Observable.fromEvent(btn, 'click')
+const txtStream$  = Observable.fromEvent(txt, 'keyup')
 
 btnStream$.subscribe(
     e => console.log(e, 'Clicked'),
+    err => console.log(err),
+    () => console.log('Completed')
+)
+
+txtStream$.subscribe(
+    e => console.log(e.currentTarget.value),
     err => console.log(err),
     () => console.log('Completed')
 )
