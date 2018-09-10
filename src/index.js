72541f92f162fd19cf9d00221a8fe90729a955ea
alex.fan <alex.fan@okchem.com>
2018-09-10 19:22:19 +0800

    pluck

--- src/index.js
+++ src/index.js
@@ -11,11 +11,46 @@ import $ from 'jquery'
 //   v => console.log(v)
 // )
 
+/* map * 2 */
+// const source$  = Observable.from(['Jone', 'Tom', 'Shawn'])
+//       .map(v => v.toUpperCase())
+//       .map(v => `I am ${v}`)
+//
+// source$.subscribe(
+//   v => console.log(v)
+// )
 
-const source$  = Observable.from(['Jone', 'Tom', 'Shawn'])
-      .map(v => v.toUpperCase())
-      .map(v => `I am ${v}`)
+/* map * 2 */
+// const getGithubUser = username => $.ajax({
+//   url: `https://api.github.com/users/${username}`,
+//   dateType: 'jsonp'
+// }).promise()
+//
+// Observable.fromPromise(getGithubUser('fancn21th'))
+//   .map(user => user.name)
+//   .subscribe(
+//     v => console.log(v)
+//   )
 
-source$.subscribe(
+/* pluck */
+
+const users = [
+  {
+    name: 'Will',
+    age: '34'
+  },
+  {
+    name: 'Mike',
+    age: '37'
+  },
+  {
+    name: 'Tony',
+    age: '36'
+  },
+]
+
+const users$ = Observable.from(users).pluck('name')
+
+users$.subscribe(
   v => console.log(v)
 )
