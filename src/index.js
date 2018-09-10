4084b7b49755cfdee489f1efc548493f20819503
alex.fan <alex.fan@okchem.com>
2018-09-10 19:37:35 +0800

    merge map

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 import { Observable } from 'rxjs'
 import $ from 'jquery'
 
-/* merge */
+/* merge map */
 
-/* common syntax */
-
+// wrong way
 // Observable.of('Hello')
-//           .merge(Observable.of('Everyone'))
-//           .subscribe(v => console.log(v))
-//
-// Observable.interval(2000)
-//           .merge(Observable.interval('500'))
-//           .take(25)
-//           .subscribe(v => console.log(v))
+//           .subscribe(v => {
+//               Observable.of(`${v} Everyone`)
+//                         .subscribe(x => console.log(x))
+//           })
 
-/* short syntax */
+// right way
+Observable.of('Hello')
+          .mergeMap(v => {
+              return Observable.of(`${v} Everyone`)
+          })
+          .subscribe(v => console.log(v))
 
-// const source1$ = Observable.interval(2000).map(v => `Merge 1 : ${v}`)
-// const source2$ = Observable.interval(200).map(v => `Merge 2 : ${v}`)
-//
-// Observable.merge(source1$, source2$).take(25).subscribe(v => console.log(v))
-
-
-/* concat */
-
-const source1$ = Observable.range(0, 5).map(v => `Source 1 : ${v}`)
-const source2$ = Observable.range(6, 5).map(v => `Source 2 : ${v}`)
-
-Observable.concat(source1$, source2$).subscribe(v => console.log(v))
+/* switch map */
